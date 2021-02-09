610e91d2aff847bfb55ba91f8636904eb2039dcd
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-09 16:58:01 -0500

    add initial CharactersPages component and connect

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CharactersPages from '../../containers/CharactersPages';
 import Header from '../header/Header';
 
 export default function App() {
@@ -8,9 +9,9 @@ export default function App() {
       <Header />
       <Switch>
 
-        <Route exact path="/" component={Header} />
+        <Route exact path="/" component={CharactersPages} />
 
-        {/* <Route exact path="" component={} /> */}
+        {/* <Route exact path="/" component={CharactersPages} /> */}
 
       </Switch>
     </Router>
---
+++ src/containers/CharactersPages.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function CharactersPages() {
+  
+  
+    return (
+    <div>
+            Hello from pages
+    </div>
+  );
+}
