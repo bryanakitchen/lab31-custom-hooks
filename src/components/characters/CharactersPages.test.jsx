858b835484c9ae71fe81601de338d488d7f363c8
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-10 10:57:38 -0500

    test is red - expect a list of characters

--- src/components/characters/CharactersPages.test.jsx
+++ src/components/characters/CharactersPages.test.jsx
@@ -3,9 +3,11 @@ import { render, screen, waitFor } from '@testing-library/react';
 import CharactersPages from './CharactersPages';
 
 describe('CharactersPages container', () => {
-  it('fetches and displays a list of characters', () => {
+  it('fetches and displays a list of characters', async() => {
     render(<CharactersPages />);
 
     screen.getByAltText('loading');
+
+    const listOfCharacters = await screen.findByTestId('characters');
   });
 });
