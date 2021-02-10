99e2663412f9328c98b5580f0f5b8616a11f7436
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-10 11:24:21 -0500

    mock server and connect json data

--- src/containers/CharactersPages.test.jsx
+++ src/containers/CharactersPages.test.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import CharactersPages from '../containers/CharactersPages';
 import { MemoryRouter } from 'react-router-dom';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import charactersAPIResponse from '../fixtures/characters.json';
+
+const server = setupServer(
+  rest.get('https://ac-vill.herokuapp.com/villagers', (req, res, ctx) => {
+    return res(ctx.json(charactersAPIResponse));
+  })
+);
 
 describe('CharactersPages container', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+  
   it('fetches and displays a list of characters', async() => {
     render(<MemoryRouter><CharactersPages /></MemoryRouter>);
 
